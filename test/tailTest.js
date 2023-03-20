76c354066a6966b335b0361ec600cea64f3c8333
Asma Jama <asmajama98@gmail.com>
2023-03-20 00:26:58 -0400

    Refactored test code with Mocha/Chai

---
+++ test/tailTest.js
@@ -0,0 +1,10 @@
+const assertEqual = require('../assertEqual');
+const tail = require('../tail');
+const assert = require('chai').assert;
+
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+      assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+    });
+});
+
