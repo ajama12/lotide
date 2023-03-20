b14d7cb89cb99fe24e6e3d358c220df1f52e174a
Asma Jama <asmajama98@gmail.com>
2023-03-19 22:42:58 -0400

    Refactored and removed test code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -6,21 +6,11 @@ const assertArraysEqual = function(arr1, arr2) {
   } else {
     result += `🛑 Assertion Failed: [${arr1}] === [${arr2}]`;
   }
-  console.log(result);
+  return result;
 };
 
+const eqArrays = require('./eqArrays');
 
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+module.exports = assertArraysEqual;
 
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
-console.log(assertArraysEqual([1, 4, 4], [1, 4, 3]))
+
