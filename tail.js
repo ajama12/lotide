1cbc0aa9da48cbf102627e49c0d62c070f70dfb6
Asma Jama <asmajama98@gmail.com>
2023-03-19 22:22:09 -0400

    Refactored and removed test code

--- tail.js
+++ tail.js
@@ -1,15 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const tail = function(array) {
   return array.slice(1);
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+module.exports = tail;
