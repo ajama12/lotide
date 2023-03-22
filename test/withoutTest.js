df882491cdb661ab70e97ab490ec29aa3b861106
Asma Jama <asmajama98@gmail.com>
2023-03-21 21:13:09 -0400

    Refactored and added test code

---
+++ test/withoutTest.js
@@ -0,0 +1,12 @@
+const assertArraysEqual = require("../assertArraysEqual");
+const eqArrays = require("../eqArrays");
+const without = require("../without");
+
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+console.log(assertArraysEqual(words, ["hello", "world", "lighthouse"]));
+
